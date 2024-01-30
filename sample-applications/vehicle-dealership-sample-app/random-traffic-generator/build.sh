0507fc67fb23ed3bae685f40d5d94a0b2b4c3887
Zhonghao Zhao <zzhlogin@amazon.com>
2024-01-30 18:14:49 +0000

    Add application module into PYTHONPATH, all is good now.

--- sample-applications/vehicle-dealership-sample-app/random-traffic-generator/build.sh
+++ sample-applications/vehicle-dealership-sample-app/random-traffic-generator/build.sh
@@ -11,11 +11,11 @@ endpoint=$(kubectl get svc -n ingress-nginx | grep "ingress-nginx" | awk '{print
 echo "Endpoint: $endpoint"
 
 export REPOSITORY_PREFIX=${account}.dkr.ecr.${region}.amazonaws.com
-#aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${REPOSITORY_PREFIX}
-#aws ecr create-repository --repository-name random-traffic-generator --region ${region} || true
-#docker build -t random-traffic-generator:latest .
-#docker tag random-traffic-generator:latest ${REPOSITORY_PREFIX}/random-traffic-generator:latest
-#docker push ${REPOSITORY_PREFIX}/random-traffic-generator:latest
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${REPOSITORY_PREFIX}
+aws ecr create-repository --repository-name random-traffic-generator --region ${region} || true
+docker build -t random-traffic-generator:latest .
+docker tag random-traffic-generator:latest ${REPOSITORY_PREFIX}/random-traffic-generator:latest
+docker push ${REPOSITORY_PREFIX}/random-traffic-generator:latest
 
 sed  -e 's#\${REPOSITORY_PREFIX}'"#${REPOSITORY_PREFIX}#g" -e 's#\${URL}'"#$endpoint#g" deployment.yaml | kubectl apply -f -
 
