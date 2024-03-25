f1a7bec4e68915d0028fa73006ab9d939615c8dd
XinRan Zhang <xinranzh@amazon.com>
2024-03-25 18:39:32 +0000

    arch test script

---
+++ scripts/download_and_install_distro.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Fail fast
+set -e
+
+# Check script is running in contract-tests
+current_path=`pwd`
+current_dir="${current_path##*/}"
+if [ "$current_dir" != "aws-otel-python-instrumentation" ]; then
+  echo "Please run from aws-otel-python-instrumentation dir"
+  exit
+fi
+
+# Setup - update dependencies and create/empty dist dir
+python3 -m pip install --upgrade pip setuptools wheel packaging build
+mkdir -p dist
+rm -rf dist/aws_opentelemetry_distro*
+
+# Download distro from PyPI
+# Specify the version of aws-opentelemetry-distro you want to download
+VERSION="0.0.1"
+pip download aws-opentelemetry-distro==$VERSION -d ./dist
+
+# Install distro
+cd dist
+DISTRO=(aws_opentelemetry_distro-*-py3-none-any.whl)
+python3 -m pip install $DISTRO --force-reinstall
+cd ..
